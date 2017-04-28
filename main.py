96c9d20b2c4aa1c7f18bb6f2360b2ab26431537e
matCsikai <ccsmatyas@gmail.com>
2017-04-28 23:36:59 +0200

    working add story function

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request
+import csv
 app = Flask(__name__)
 
 
-@app.route('/story')
-@app.route('/story/<story_id>')
-def story(story_id=None):
-    return render_template("form.html", story_id=story_id)
+@app.route('/story', methods=['POST', 'GET'])
+def save(story_id=None):
+    if request.method == 'POST':
+        story_title = request.form["Story title"]
+        user_story = request.form["User Story"]
+        acceptanc_criteria = request.form["Acceptance criteria"]
+        business_value = request.form["Business Value"]
+        estimation = request.form["Estimation"]
+        status = request.form["Status"]
+        fieldnames = ["Story title", "User Story", "Acceptance criteria", "Business Value", "Estimation", "Status"]
+        with open("data.csv", "a") as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writerow({"Story title": story_title, "User Story": user_story, "Acceptance criteria":
+                            acceptanc_criteria, "Business Value": business_value, "Estimation": estimation,
+                            "Status": status})
+        return render_template("form.html", story_id=story_id)
     
 
 @app.route('/list', methods=['POST', 'GET'])
