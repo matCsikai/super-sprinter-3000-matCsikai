851a501a6b4a54ab1cae6ab70cd220d9a43fbd0d
matCsikai <ccsmatyas@gmail.com>
2017-05-01 09:56:47 +0200

    comments added, slight code change

--- main.py
+++ main.py
@@ -8,25 +8,27 @@ app = Flask(__name__)
 @app.route('/story/<int:story_id>', methods=['GET', 'POST'])
 def story(story_id=None):
     database_as_list = common.data_list_with_id()
+    # open empty add story form template
     if story_id is None:
         return render_template("form.html", story_id=story_id)
     else:
         for data_row in database_as_list:
+            # open story form template to edit
             if story_id == data_row[0]:
                 data_for_edit = data_row
                 return render_template("form.html", story_id=story_id, data_for_edit=data_for_edit)
+            # return to list template if ID not exist
             else:
                 return render_template("list.html", database_as_list=database_as_list)
-            
-        
 
 
 @app.route('/list', methods=['GET', 'POST'])
-def add_to_data_file():
+def list_manager():
+    # add story input to data.csv
     if request.method == 'POST':
         story_title = request.form["Story title"]
         user_story = request.form["User Story"]
-        acceptanc_criteria = request.form["Acceptance criteria"]
+        acceptance_criteria = request.form["Acceptance criteria"]
         business_value = request.form["Business Value"]
         estimation = request.form["Estimation"]
         status = request.form["Status"]
@@ -34,12 +36,11 @@ def add_to_data_file():
         with open("data.csv", "a") as file:
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writerow({"Story title": story_title, "User Story": user_story, "Acceptance criteria":
-                            acceptanc_criteria, "Business Value": business_value, "Estimation": estimation,
+                            acceptance_criteria, "Business Value": business_value, "Estimation": estimation,
                             "Status": status})
-        database_as_list = common.data_list_with_id()
-        return render_template("list.html", database_as_list=database_as_list)
-
-
+    # send data with IDs to list template
+    database_as_list = common.data_list_with_id()
+    return render_template("list.html", database_as_list=database_as_list)
 
 
 if __name__ == "__main__":
