455e4455f0771ae428d89d9e693b98688f1741a1
matCsikai <ccsmatyas@gmail.com>
2017-05-01 20:21:08 +0200

    comment modification

--- common.py
+++ common.py
@@ -6,30 +6,30 @@ def data_import():
     with open("data.csv") as csv_file:
         imported_data = csv.reader(csv_file)
         database_as_list = [row for row in imported_data]
-    #print(database_as_list)
     return database_as_list
 
 
-# ad ID to data rows
+# adds ID to data rows
 def data_list_with_id():
     id_values = []
     database_as_list = data_import()
     for numbers in range(len(database_as_list)):
         id_values.append(numbers + 1)
-    #print(id_values)
     index = 0
     for data_row in database_as_list:
         data_row.insert(0, id_values[index])
         index += 1
-    #print(database_as_list)
     return database_as_list
 
 
-# delete data row by ID
+# deletes data row by ID (defunct)
 def delete_row(story_id):
     database_as_list = data_import()
     database_as_list.remove(database_as_list[story_id - 1])
-    #print(database_as_list)
+    return database_as_list
+
+
+
 
 
 
