f25682d2bc069325fc85c25596d7d1c9b88d9404
matCsikai <ccsmatyas@gmail.com>
2017-05-01 12:31:27 +0200

    /list, /story, /story/id working

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ def story(story_id=None):
     if story_id is None:
         return render_template("form.html", story_id=story_id)
     else:
-        for data_row in database_as_list:
+        for data_list in database_as_list:
             # open story form template to edit
-            if story_id == data_row[0]:
-                data_for_edit = data_row
+            if story_id == data_list[0]:
+                data_for_edit = data_list
                 return render_template("form.html", story_id=story_id, data_for_edit=data_for_edit)
             # return to list template if ID not exist
+            elif story_id != data_list[0] and story_id <= len(database_as_list):
+                continue
             else:
                 return render_template("list.html", database_as_list=database_as_list)
 
@@ -25,6 +27,7 @@ def story(story_id=None):
 @app.route('/list', methods=['GET', 'POST'])
 def list_manager():
     # add story input to data.csv
+    database_as_list = common.data_list_with_id()
     if request.method == 'POST':
         story_title = request.form["Story title"]
         user_story = request.form["User Story"]
@@ -38,9 +41,10 @@ def list_manager():
             writer.writerow({"Story title": story_title, "User Story": user_story, "Acceptance criteria":
                             acceptance_criteria, "Business Value": business_value, "Estimation": estimation,
                             "Status": status})
+        return render_template("list.html", database_as_list=database_as_list)
     # send data with IDs to list template
-    database_as_list = common.data_list_with_id()
-    return render_template("list.html", database_as_list=database_as_list)
+    else:
+        return render_template("list.html", database_as_list=database_as_list)
 
 
 if __name__ == "__main__":
